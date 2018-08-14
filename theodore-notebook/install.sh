a994b6855d9b69d1605053b8ec9075f8581cc1b7
Dwight Townsend <townsenddw@gmail.com>
2018-08-14 12:58:55 -0700

    Theodore and stuff

--- theodore-notebook/install.sh
+++ theodore-notebook/install.sh
@@ -4,7 +4,7 @@
 
 ## Conda Dependencies
 # Use conda to install into base environment
-conda env update -n=base -f environment.yml
+#conda env update -n=base -f environment.yml
 # If installing into a new conda env
 # Use: conda env create -f environment.yml
 # AND change the SITEPACKAGES var below to that location
@@ -14,7 +14,7 @@ SITEPACKAGES=${SITEPACKAGES:-/opt/conda/lib/python2.7/site-packages}
 git clone https://github.com/orbkit/orbkit.git 
 mv orbkit $SITEPACKAGES/orbkit 
 cd $SITEPACKAGES/orbkit
-python setup.py build_ext --inplace cleand
+python setup.py build_ext --inplace clean
 echo './orbkit' > $SITEPACKAGES/orbkit.pth
 
 ## TheoDORE Install
@@ -23,6 +23,6 @@ wget https://sourceforge.net/projects/theodore-qc/files/TheoDORE_1.7.2.tgz/downl
 tar -xf download
 chown 1000:100 TheoDORE_1.7.2
 rm -rf download work
-echo '~/TheoDORE_1.7.2/bin' > ../theodore.pth
-echo '~/TheoDORE_1.7.2/lib' >> ../theodore.pth
+echo '~/TheoDORE_1.7.2/bin' > $SITEPACKAGES/theodore.pth
+echo '~/TheoDORE_1.7.2/lib' >> $SITEPACKAGES/theodore.pth
 echo 'source TheoDORE_1.7.2/setpaths.bash' >> ~/.bashrc
